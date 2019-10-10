58c2ffa99844404ee22ff96cd793e4ceb06a8a53
Zuluft <giodzotsenidze@gmail.com>
2019-10-10 11:21:00 +0400

    rx room added

--- mvvm/src/main/java/com/zuluft/mvvm/common/extensions/ObservebleExtensions.kt
+++ mvvm/src/main/java/com/zuluft/mvvm/common/extensions/ObservebleExtensions.kt
@@ -1,12 +1,8 @@
 package com.zuluft.mvvm.common.extensions
 
-import android.os.Handler
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposables
 import io.reactivex.schedulers.Schedulers
-
-const val DEFAULT_POST_DELAY = 500L
 
 fun <T : Any> Observable<T>.async(): Observable<T> {
     return this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
@@ -14,20 +10,4 @@ fun <T : Any> Observable<T>.async(): Observable<T> {
 
 fun <T : Any> T.just(): Observable<T> {
     return Observable.just(this)
-}
-
-fun postDelayed(duration: Long = DEFAULT_POST_DELAY): Observable<Boolean> {
-    return Observable.create { emitter ->
-        val handler = Handler()
-
-        val action = {
-            if (!emitter.isDisposed) {
-                emitter.onNext(true)
-                emitter.onComplete()
-            }
-        }
-
-        handler.postDelayed(action, duration)
-        emitter.setDisposable(Disposables.fromAction { handler.removeCallbacks(action) })
-    }
 }
