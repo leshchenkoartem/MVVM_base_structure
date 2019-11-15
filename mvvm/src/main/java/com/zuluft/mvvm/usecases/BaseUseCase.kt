56d8c5f5b5e9cf355016c11f8181b906777edea9
George Dzotsenidze <gdzotsenidze@tbcbank.com.ge>
2019-11-15 12:55:06 +0400

    BaseUseCaseWithRepo abstract class added

--- mvvm/src/main/java/com/zuluft/mvvm/usecases/BaseUseCase.kt
+++ mvvm/src/main/java/com/zuluft/mvvm/usecases/BaseUseCase.kt
@@ -1,7 +1,5 @@
 package com.zuluft.mvvm.usecases
 
-abstract class BaseUseCase<REPOSITORY, ARG_TYPE, RETURN_TYPE>
-constructor(protected val repository: REPOSITORY) {
-
-    abstract fun start(arg: ARG_TYPE? = null): RETURN_TYPE
+interface BaseUseCase<ARG_TYPE, RETURN_TYPE> {
+    fun start(arg: ARG_TYPE? = null): RETURN_TYPE
 }
