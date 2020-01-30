898b31482e1a930be9e4b20276f704f3c49892ec
George Dzotsenidze <gdzotsenidze@tbcbank.com.ge>
2020-01-30 12:02:53 +0400

    gradle libs versions updated

--- mvvm/src/main/java/com/zuluft/mvvm/common/extensions/ObservebleExtensions.kt
+++ mvvm/src/main/java/com/zuluft/mvvm/common/extensions/ObservebleExtensions.kt
@@ -1,11 +1,15 @@
 package com.zuluft.mvvm.common.extensions
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-fun <T : Any> Observable<T>.async(): Observable<T> {
-    return this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+fun <T : Any> Observable<T>.async(
+    subscribe: Scheduler = Schedulers.newThread(),
+    observe: Scheduler = AndroidSchedulers.mainThread()
+): Observable<T> {
+    return this.subscribeOn(subscribe).observeOn(observe)
 }
 
 fun <T : Any> T.just(): Observable<T> {
